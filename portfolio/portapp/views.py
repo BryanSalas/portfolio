3c97d97624db70843292e82a8141e938579af9aa
Bryan Salas <bryans5252@sbcglobal.net>
2014-08-03 23:01:37 +0000

    added basic templates for all pages

--- portfolio/portapp/views.py
+++ portfolio/portapp/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {'right_now':datetime.utcnow()})
+
+def bio(request) :
+    return render(request, 'bio.html')
+
+def projects(request) :
+    return render(request, 'projects.html')
+
+def contact(request) :
+    return render(request, 'contact.html')
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,10 +6,13 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'portfolio.portapp.views.home', name='home'),
+    url(r'^bio/', 'portfolio.portapp.views.bio', name='bio'),
+    url(r'^projects/', 'portfolio.portapp.views.projects', name='projects'),
+    url(r'^contact/', 'portfolio.portapp.views.contact', name='contact'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls))
 )
